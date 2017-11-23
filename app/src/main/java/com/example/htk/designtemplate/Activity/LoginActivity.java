5051a961a03f93233aac2eebefeb46f065d79dc5
Nguyễn Tuấn <nguyentuanit96@gmail.com>
2017-11-23 22:06:48 +0700

    Cập nhật lại màn hình Login, Sign-up, Post

--- app/src/main/java/com/example/htk/designtemplate/Activity/LoginActivity.java
+++ app/src/main/java/com/example/htk/designtemplate/Activity/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.htk.designtemplate.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.htk.designtemplate.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
